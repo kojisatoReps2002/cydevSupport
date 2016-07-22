23aa43ac44c8d7837e1f82bee5f306a6d51d58ab
Koji Sato <kojisato2002@gmail.com>
2016-07-22 17:49:51 +0900

    とりあえず、更新できるように再構築

--- UpdateParentRecord.js
+++ UpdateParentRecord.js
@@ -4,35 +4,36 @@
 
 (function() {
   "use strict";
-  // Register the events.
-  var events = ['app.record.create.submit']
+  //登録ボタン押下時に発火
+  kintone.events.on(['app.record.create.submit'], function(event) {
+    var related = kintone.app.getLookupTargetAppId('見積番号_lookup');
+    var estimateId = parseInt(event.record['見積書レコード番号'].value, 10);
+    var requestParam = { "app": related, "id" : estimateId };
+    var subTotal = parseInt(event.record['小計'].value, 10);
 
-  kintone.events.on(events, function(event) {
-    var app_code = "EstimateAppCode001";        // 見積書アプリコード
-    kintone.api('/k/v1/apps', 'GET', {"codes": [ app_code ] }, function (resp) {
-      var app_id = resp.apps[0].appId;
-      var record = event.record;
-      var rec_id = record['見積書レコード番号']['value'];
-      var amount = parseInt(record['小計']['value']) + resp.apps[0].見積金額;
- 
-      // 見積書-見積金額更新リクエスト
-      kintone.api('/k/v1/record', 'PUT', {
-        "app": app_id, "id": rec_id, "revision": -1, "record":{
-        "見積金額" : { "value": amount } }
-      }, function(resp) {
-      }, function(resp) {
-        var errmsg = 'An error occurred while updating records.';
-        if (resp.message !== undefined) {
-          errmsg += '\n' + resp.message;
-          alert(errmsg);
-        }
-      });
-    }, function (resp) {
-      var errmsg = 'An error occurred while getting records.';
-      if (resp.message !== undefined) {
-        errmsg += '\n' + resp.message;
-        alert(errmsg);
+    kintone.api(kintone.api.url('/k/v1/record', true), 'GET', requestParam,
+      function(resp) {
+        var amount = parseInt(resp.record['見積金額'].value, 10) + subTotal;
+        var revision = parseInt(resp.record['$revision'].value, 10);
+
+        kintone.api('/k/v1/record', 'PUT', {
+
+          "app": related, 
+          "id": estimateId, 
+          "revision": revision, 
+          "record":{"見積金額" : { "value": amount }}
+
+        }, function(resp) {
+        }, function(resp) {
+          var errmsg = 'An error occurred while updating records.';
+          if (resp.message !== undefined) {
+            errmsg += '\n' + resp.message;
+            alert(errmsg);
+          }
+        }); 
+      }, function(error) {
+        console.log(error);
       }
-    });
+    );
   });
 })();
